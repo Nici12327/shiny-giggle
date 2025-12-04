ffe62e9c6e84833f243c6372010a2604492f06f1
Nici12327 <nicolas.ott09@icloud.com>
2025-12-04 15:18:20 +0100

    Update token.py

--- data/token.py
+++ data/token.py
@@ -1,1 +1,8 @@
-token='{insert token here}'
+"""Deprecated placeholder to avoid accidental imports.
+
+This file used to contain the bot token but has been replaced by
+`data/credentials.py`. Do NOT import this module; it shadows the
+standard library `token` module if imported incorrectly.
+"""
+
+token = None
