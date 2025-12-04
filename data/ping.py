fda1a1f0d1025b683c436894621b52a1d855bf93
Nici12327 <nicolas.ott09@icloud.com>
2025-12-04 14:46:32 +0100

    Create ping.py

---
+++ data/ping.py
@@ -0,0 +1,7 @@
+import discord
+from discord import app_commands
+
+def register(tree: app_commands.CommandTree):
+    @tree.command(name='ping', description='Check if bot is online/running correctly')
+    async def ping(interaction: discord.Interaction):
+        await interaction.response.send_message('Pong')
